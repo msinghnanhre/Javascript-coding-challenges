246356ece9ef54f0b25116ca8f141a07c6cb1d0e
Manjinder <smanjinder96.ms@gmail.com>
2021-07-25 14:40:03 -0700

    max consecutive 1's

---
+++ arrays/consecutive.js
@@ -0,0 +1,26 @@
+//Given a binary array nums, return the maximum number of consecutive 1's in the array.
+//Input: nums = [1,1,0,1,1,1]
+//Output: 3
+//Explanation: The first two digits or the last three digits are consecutive 1s.The maximum number of consecutive 1s is 3.
+
+let nums = [1, 1, 1, 1, 0, 1, 1, 1]
+
+let nums2 = [1, 1, 1, 1, 0, 1, 1, 1, 1, 1]
+
+const consecutive = (arr) => {
+    let count = 0
+    let maxResult = 0
+    arr.map(item => {
+        if (item === 1) {
+            count ++
+            maxResult = Math.max(maxResult, count)
+        }
+        else {
+            count = 0
+        }
+        return maxResult
+    })
+    return maxResult
+}
+
+console.log(consecutive(nums2))
--- arrays/flattenArray.js
+++ arrays/flattenArray.js
@@ -1,18 +1,20 @@
 //flatten the given array into single array.
-let array = [1, 2, , 3, 4, [5, 6, 7], [8, 9, 10]]
+let array = [1, 2, , 3, 4, [5, 6, 7], [8, 9, [4,5,2], 10]]
 
 let flattenArray = (arr) => {
     let newFlattenArray = arr.reduce((acc, item) => {
 
         //if item itself is an array
         if (Array.isArray(item)) {
-            //if item is array run the same function on item
+            //run the same function on item
             acc = acc.concat(flattenArray(item))
         }
         
         //if item is single item
         else {
             acc.push(item)
+            //alternatively
+            //acc = [...acc, item]
         }
         return acc
     }, [])
