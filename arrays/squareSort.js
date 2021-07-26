08918ae9d6862731c8d7a3479c5478611b5f3e40
Manjinder <smanjinder96.ms@gmail.com>
2021-07-26 12:15:50 -0700

    square array ,return sorted arr

---
+++ arrays/squareSort.js
@@ -0,0 +1,18 @@
+//Given an integer array nums sorted in non-decreasing order,
+//return an array of the squares of each number sorted in non - decreasing order
+
+// Input: nums = [-4, -1, 0, 3, 10]
+// Output: [0, 1, 9, 16, 100]
+// Explanation: After squaring, the array becomes[16, 1, 0, 9, 100].
+// After sorting, it becomes[0, 1, 9, 16, 100].
+
+nums = [-4, -1, 0, 3, 10]
+
+const squareSort = (num) => {
+    let squareArr = num.map(item => {
+       return item*item
+    })
+    return squareArr.sort((a,b)=> a-b)
+}
+
+console.log(squareSort(nums))
