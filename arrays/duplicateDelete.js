e708306f3df58f61c79d64c94d4e904fdd4b5b71
Manjinder <smanjinder96.ms@gmail.com>
2021-08-09 08:03:15 -0700

    delete duplicates using lastIndexOf method

--- arrays/duplicateDelete.js
+++ arrays/duplicateDelete.js
@@ -2,31 +2,20 @@
 // Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k(hence they are underscores).
-let nums = [1, 1, 2]
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 const deleteDuplicates = (nums) => {
-    // let newNum = num.slice()
-    // nums = [...new Set(newNum)]
-    // return nums
-    // num = num.filter((value, index) => {
-    //     return num.indexOf(value) === index
-    // })
-    // return num
-    return nums = [...new Set(nums)]
+    return nums.map((item, index) => {
+        if (index !== nums.lastIndexOf(item) ) {
+            nums.splice(index, nums.lastIndexOf(item)-index)
+        }
+        return item
+    })
+    // nums = [...new Set(nums)]
+    // return nums.length
 }
 
 console.log(deleteDuplicates(nums))
+// nums.splice(2, 2)
+// console.log(nums)
 
-//Try Alternatives
-
-//return unique values in arr 
-//newArr.filter((x, i, a) => a.indexOf(x) == i)
-
-// let newArr = []
-// num.map((item, index) => {
-//     if (newArr.indexOf(item) === -1) {
-//         newArr.push(item)
-//     } else if (newArr.indexOf(item) !== index) {
-//         num.splice(index, 1)
-//     }
-// })
-// return num
+//console.log(nums.lastIndexOf(1))
